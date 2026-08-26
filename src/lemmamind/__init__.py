829cde3769c70b6327cdc3872cf36d6d1e9006f8
Alajmah <55954113+Alajmah@users.noreply.github.com>
2026-08-26 04:25:32 +0300

    Register M5 change contracts on package import

--- src/lemmamind/__init__.py
+++ src/lemmamind/__init__.py
@@ -1,11 +1,21 @@
 """LemmaMind executable contracts and persistence."""
 
+from .change_contracts import (
+    ArtifactDelta,
+    ArtifactDeltaType,
+    StructuralDelta,
+    StructuralDeltaType,
+)
 from .contracts import CONTRACT_SCHEMA_VERSION
 from .tracking_contracts import RepositoryTrackingAssignment, TrackingLevel
 
 __all__ = [
+    "ArtifactDelta",
+    "ArtifactDeltaType",
     "CONTRACT_SCHEMA_VERSION",
     "RepositoryTrackingAssignment",
+    "StructuralDelta",
+    "StructuralDeltaType",
     "TrackingLevel",
 ]
 __version__ = "0.1.0"
